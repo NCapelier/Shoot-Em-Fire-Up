88da959b94a2c29617fb98de0b6ec22c3693c8e0
Nicolas Capelier <capelier.nicolas@gmail.com>
2020-02-14 16:10:53 +0100

    Added player shoot & world border

---
+++ Shoot Em Fire Up/Assets/Scripts/Player/PlayerShoot.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Game
+{
+    public class PlayerShoot : MonoBehaviour
+    {
+        //shoot cooldown variables
+        bool canShoot = true;
+        [Range(0.1f, 5f)]
+        [SerializeField] float cooldownTime = 1f;
+        
+        void Awake()
+        {
+            
+        }
+        
+        void Start()
+        {
+            
+        }
+        
+        void Update()
+        {
+            Shoot();
+        }
+        
+        void Shoot()
+        {
+            if(Input.GetKey(KeyCode.Space) && canShoot)
+            {
+                StartCoroutine(ShootCooldown());
+                Instantiate(Resources.Load("Prefabs/Player/Bullet"), gameObject.transform.position, Quaternion.identity);
+            }
+        }
+
+        IEnumerator ShootCooldown()
+        {
+            canShoot = false;
+            yield return new WaitForSeconds(cooldownTime);
+            canShoot = true;
+        }
+        
+    }
+}
