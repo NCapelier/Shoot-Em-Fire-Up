b3126ec6d805a61ad498522f10992d3b541327c9
Lefebvre <Louis>
2020-02-14 16:11:10 +0100

    prefab added

---
+++ Shoot Em Fire Up/Assets/Scripts/tirs/TirsPrefabBehaiviour.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TirsPrefabBehaiviour : MonoBehaviour
+{
+    [Range(0f, 100f)]
+    [SerializeField]
+    private float speed = 1f;
+    Rigidbody2D rb;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        rb.velocity = Vector2.up*speed;
+    }
+}
