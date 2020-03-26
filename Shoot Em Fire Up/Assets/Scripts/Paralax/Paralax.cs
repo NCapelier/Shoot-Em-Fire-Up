36cd5b9b302628db806dd706a4ba17a15ca491b4
Arthur <arthurgpro@outlook.fr>
2020-03-26 14:44:05 +0100

    Background paralax

---
+++ Shoot Em Fire Up/Assets/Scripts/Paralax/Paralax.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Paralax : MonoBehaviour
+{
+    public float picLength;
+    private float picSpeed;
+    public float parallaxSpeed;
+
+    void Start()
+    {
+        //picLength = GetComponent<SpriteRenderer>().bounds.size.y;
+        Debug.Log(picLength);
+    }
+
+    void Update()
+    {
+        picSpeed = -parallaxSpeed * Time.fixedTime;
+        transform.position = new Vector2(transform.position.x, Mathf.Repeat(picSpeed, picLength));
+    }
+}
